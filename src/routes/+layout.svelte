0105cc1afb0ee190be0cdbefb32579b30ac6a57e
rumor1993 <rumor1993@naver.com>
2023-06-16 01:55:06 +0900

    vercel 설정 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Header from './Header.svelte';
 	import "../app.css";
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="app">
