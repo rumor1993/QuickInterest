239aec6ab7bbf4130aa436a567a1d62421b05969
rumor1993 <rumor1993@naver.com>
2023-07-14 23:33:06 +0900

    애드핏 광고 컴포넌트 생성 완료!

--- src/routes/custom/Adfit.svelte
+++ src/routes/custom/Adfit.svelte
@@ -2,22 +2,17 @@
     import {onMount} from "svelte";
 
     onMount(() => {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+
         const ins = document.createElement('ins');
         const script = document.createElement('script');
 
         ins.className = 'kakao_ad_area';
         ins.style.display = 'none;';
 
-        const winodwSize = window.innerWidth;
-        if (winodwSize < 1024) {
-            ins.setAttribute('data-ad-width', '320');
-            ins.setAttribute('data-ad-height', '50');
-            ins.setAttribute('data-ad-unit', 'DAN-biusmlMB8b29bjEK');
-        } else {
-            ins.setAttribute('data-ad-width', '728');
-            ins.setAttribute('data-ad-height', '90');
-            ins.setAttribute('data-ad-unit', '728x90 사이즈의 광고 ID');
-        }
+        ins.setAttribute('data-ad-width', '320');
+        ins.setAttribute('data-ad-height', '50');
+        ins.setAttribute('data-ad-unit', 'DAN-biusmlMB8b29bjEK');
 
         script.async = true;
         script.type = 'text/javascript';
